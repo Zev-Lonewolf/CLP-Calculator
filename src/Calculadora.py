c020ab598b1379b9e302139d21c5b2e49734beab
Zev Lonewolf <gleidsongonzagasilva@gmail.com>
2025-07-14 10:59:15 -0400

    Final Uptade CLP Calculator 2025.07.14_007
    Final code of the CLP Calculator, it is likely that in the future it will undergo updates to acommodate more operations and corrections.

--- src/Calculadora.py
+++ src/Calculadora.py
@@ -1,43 +1,57 @@
-valores = []
-operacoes = []
-
 while True:
+    print('\n---------------------')
+    valores = []
+    operacoes = []
+
     try:
-        valor = float(input('Valor: '))
-        valores.append(valor)
-    except ValueError:
-        print('Valor Inválido! Insira um número válido.')
-        continue
-
-    operação = input('Operação ou OK para sair: ').strip().lower()
-
-    if operação == 'ok':
-        break
-    elif operação in ['+', '-', '*', '/']:
-        operacoes.append(operação)
-    else:
-        print('Operação inválida!')
-        valores.pop()
-        continue
-resultado = valores[0]
-
-for i in range(len(operacoes)):
-    op = operacoes[i]
-    prox_valor = valores[i + 1]
-
-    if op == '+':
-        resultado += prox_valor
-    elif op == '-':
-        resultado -= prox_valor
-    elif op == '*':
-        resultado *= prox_valor
-    elif op == '/':
-        if prox_valor != 0:
-            resultado /= prox_valor
-        else:
-            print('Erro! Divisão por zero!')
-            resultado = None
+        valor = input('Valor ou "SAIR" para encerrar: ').strip().lower()
+        if valor == 'sair':
+            print('Calculadora encerrada...')
             break
+        valores.append(float(valor))
+    except ValueError:
+        print("Valor inválido!")
+        continue
 
-if resultado is not None:
-    print({resultado})
+    while True:
+        operacao = input('Operação ou "OK" para calcular: ').strip().lower()
+        
+        if operacao == 'ok':
+            break
+        elif operacao in ['+', '-', '*', '/']:
+            operacoes.append(operacao)
+        else:
+            print('Operação inválida!')
+            continue
+
+        try:
+            valor = float(input('Valor: '))
+            valores.append(valor)
+        except ValueError:
+            print('Valor inválido!')
+            operacoes.pop()
+            continue
+
+    resultado = valores[0]
+    for i in range(len(operacoes)):
+        op = operacoes[i]
+        prox_valor = valores[i + 1]
+
+        if op == '+':
+            resultado += prox_valor
+        elif op == '-':
+            resultado -= prox_valor
+        elif op == '*':
+            resultado *= prox_valor
+        elif op == '/':
+            if prox_valor != 0:
+                resultado /= prox_valor
+            else:
+                print('Erro! Divisão por zero!')
+                resultado = None
+                break
+
+    if resultado is not None:
+        print(f'\n{resultado}')
+
+#BASE DA CALCULADORA TERMINADA EM 14/07/2025! by Zev Lonewolf.
